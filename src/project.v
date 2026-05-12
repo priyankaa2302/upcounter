ae658ec615aeffc6ebed3dc356a0a578e562ff92
priyankaa2302 <priyankaa2302@gmail.com>
2026-05-12 15:32:21 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,35 +1,27 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
 module tt_um_upcounter (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-    reg [7:0] counter;
-    
-    always@(posedge clk or negedge rst_n) begin
-        if(!rst_n)
-            counter <=8'b0;
-        else
-            counter <= counter + 1'b1;
-    end
-  assign uo_out  = counter;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  reg [7:0] counter;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+      counter <= 8'b0;
+    else
+      counter <= counter + 1'b1;
+  end
+
+  assign uo_out  = counter;
+  assign uio_out = 8'b0;
+  assign uio_oe  = 8'b0;
 
 endmodule
